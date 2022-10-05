bfe12743b656bf039a83c1b0075a098b53589cfb
TiborGalambos <tibor122333@gmail.com>
2022-10-05 10:54:56 +0200

    added get birth, death date, date comparer

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import re
+from datetime import datetime
 
 small_file = "C:/Users/tibor/Desktop/FREEBASE/freebase-head-1000000"
 huge_file = "C:/Users/tibor/Desktop/FREEBASE/freebase-rdf-latest"
 
-open_this_file = small_file
-
+open_this_file = huge_file
 auto_assign_person = True
+
+person_1_data = []
+person_2_data = []
+
+# --------------------------------------------------------------------- #
+
 
 def get_persons():
     if auto_assign_person:
-        return 'Kathrin Marchand', 'Vaino Haapalainen'
+        return 'Dursley McLinden', 'Robert Brattain'
 
     else:
         print("write first name:")
@@ -22,10 +28,11 @@ def get_persons():
 
 def is_person(temp_object_data):
     for rows in temp_object_data:
-        return re.search('<http:\/\/rdf.freebase.com\/ns\/type.object.type>.*<http:\/\/rdf.freebase.com/ns/people.person>', rows)
+        return re.search(
+            '<http:\/\/rdf.freebase.com\/ns\/type.object.type>.*<http:\/\/rdf.freebase.com/ns/people.person>', rows)
 
 
-def has_name(temp_object_data):
+def get_name(temp_object_data):
     name = None
     for rows in temp_object_data:
         try:
@@ -33,22 +40,65 @@ def has_name(temp_object_data):
             return name
         except:
             pass
+    return name
 
-def check_temp_object(temp_object_data):
 
+def get_birth_date(temp_object_data):
+    birth_date = None
+    for rows in temp_object_data:
+        try:
+            birth_date = re.search('<http:\/\/rdf.freebase.com\/ns\/people.person.date_of_birth>\t\"(.*?)\"',
+                                   rows).group(1)
+            return birth_date
+        except:
+            pass
+    return birth_date
+
+
+def get_death_date(temp_object_data):
+    death_date = None
+    for rows in temp_object_data:
+        try:
+            death_date = re.search('<http:\/\/rdf.freebase.com\/ns\/people.deceased_person.date_of_death>\t\"(.*?)\"',
+                                   rows).group(1)
+            return death_date
+        except:
+            pass
+    return death_date
+
+
+def check_temp_object(temp_object_data, person_1_name, person_2_name):
     if is_person(temp_object_data):
-        # print(*temp_object_data, sep='\n')
-        # print('-------------')
-        name = has_name(temp_object_data)
-        if name != None:
-            print('name = ' + name)
 
+        name = get_name(temp_object_data)
+        if name != None and (name == person_1_name or name == person_2_name):
+            birth_date = get_birth_date(temp_object_data)
+            death_date = get_death_date(temp_object_data)
+
+            if not person_1_data:  # if empty
+                person_1_data.extend([name, birth_date, death_date])
+                print('found: \n', *person_1_data, sep='\n')
+            else:
+                person_2_data.extend([name, birth_date, death_date])
+                print('\nfound: \n', *person_2_data, sep='\n')
+                return True
+
+
+def could_they_meet():
+    person_1_birth = datetime.strptime(person_1_data[1], '%Y-%m-%d')
+    person_1_death = datetime.strptime(person_1_data[2], '%Y-%m-%d')
+
+    person_2_birth = datetime.strptime(person_2_data[1], '%Y-%m-%d')
+    person_2_death = datetime.strptime(person_2_data[2], '%Y-%m-%d')
+
+    if (person_1_birth < person_2_birth < person_1_death or person_2_birth < person_1_birth < person_2_death):
+        print('\nthey could meet')
+    else:
+        print('\nthey could not meet')
+    exit(0)
 
 
 def search(person_1_name, person_2_name):
-
-    person_1_data = []
-    person_2_data = []
     temp_object_data = []
 
     prev_object_id = ''
@@ -60,47 +110,25 @@ def search(person_1_name, person_2_name):
             fetched_object_id = re.search('^<http:\/\/rdf\.freebase\.com\/ns\/(.*?)>.*$', line).group(1)
 
             if fetched_object_id != prev_object_id:
-                # print('printing whole object', *temp_object_data, sep='\n')
-                # print('------------')
-                check_temp_object(temp_object_data)
+                if (check_temp_object(temp_object_data, person_1_name, person_2_name)):  # check prev object data
+                    could_they_meet()
                 temp_object_data.clear()
-                # print('new object found: ' + fetched_object_id)
-                current_object_id = fetched_object_id
+                current_object_id = fetched_object_id  # new object
 
-            if current_object_id == prev_object_id:
+            if current_object_id == prev_object_id:  # adding data
                 temp_object_data.append(line)
-                # print('adding data')
-
-
-
                 current_object_id = fetched_object_id
 
             prev_object_id = fetched_object_id
 
-
-            # TODO: Get the inital object id and get the rest of the lines with the same id.
-            # TODO: Check the list of lines with same object if one of the names occurs, then get the birth date, ?death date?, city.
-            # TODO: always check if the object is person
-            # if current_object:
-            #     print(line)
-
-            # match_1 = re.search(person_1_name, line)
-            # match_2 = re.search(person_2_name, line)
-            # if match_1:
-            #     result_1.append(line)
-        print('end')
-
-        # print('printing whole object', *temp_object_data, sep='\n')
-        # print('------------')
+        if (check_temp_object(temp_object_data, person_1_name, person_2_name)):  # check prev object data
+            could_they_meet()
         temp_object_data.clear()
 
-if __name__ == '__main__':
 
-    result_1 = []
-    result_2 = []
+if __name__ == '__main__':
     person_1_name, person_2_name = get_persons()
 
-    search(person_1_name, person_2_name)
+    print('searching names: {}, {} ...'.format(person_1_name, person_2_name))
 
-    print(*result_1, sep='\n')
-    print(*result_2, sep='\n')
+    search(person_1_name, person_2_name)
